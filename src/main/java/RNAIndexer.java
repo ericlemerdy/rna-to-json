aa79c31745e163f3c9068b8eb893de6246f1e3f6
Éric Le Merdy <eric.lemerdy@laposte.net>
2017-05-15 23:00:31 +0200

    Index from rna file.

--- src/main/java/RNAIndexer.java
+++ src/main/java/RNAIndexer.java
@@ -5,26 +5,68 @@ import com.fasterxml.jackson.databind.SequenceWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.google.common.annotations.VisibleForTesting;
+import data.Association;
+import data.Id;
+import data.Index;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 
 import static com.fasterxml.jackson.databind.MapperFeature.CAN_OVERRIDE_ACCESS_MODIFIERS;
 import static java.lang.String.format;
 import static java.lang.System.out;
+import static java.net.InetAddress.getLocalHost;
 import static java.nio.charset.Charset.forName;
-
-import data.Association;
-import data.Id;
-import data.Index;
+import static org.elasticsearch.action.bulk.BackoffPolicy.exponentialBackoff;
+import static org.elasticsearch.common.settings.Settings.EMPTY;
+import static org.elasticsearch.common.unit.ByteSizeUnit.MB;
+import static org.elasticsearch.common.unit.TimeValue.timeValueMillis;
+import static org.elasticsearch.common.unit.TimeValue.timeValueSeconds;
+import static org.elasticsearch.common.xcontent.XContentType.JSON;
 
 public class RNAIndexer {
     private static final char SEPARATOR_CHAR = ';';
     private static final int BUFFER_SIZE = 10000;
+    public static final BulkProcessor.Listener BULK_PROCESSOR_ADAPTER = new BulkProcessor.Listener() {
+        @Override
+        public void beforeBulk(long executionId,
+                               BulkRequest request) {
+        }
+
+        @Override
+        public void afterBulk(long executionId,
+                              BulkRequest request,
+                              BulkResponse response) {
+        }
+
+        @Override
+        public void afterBulk(long executionId,
+                              BulkRequest request,
+                              Throwable failure) {
+        }
+    };
     private CsvMapper csvMapper;
     private CsvSchema csvSchema;
+    private Client client;
 
-    public RNAIndexer() {
+
+    public RNAIndexer(InetAddress inetAddress) throws UnknownHostException {
+        this(new PreBuiltTransportClient(EMPTY)
+                .addTransportAddress(new InetSocketTransportAddress(inetAddress, 9300)));
+    }
+
+    public RNAIndexer(Client client) {
+        this.client = client;
         csvMapper = (CsvMapper) new CsvMapper().disable(CAN_OVERRIDE_ACCESS_MODIFIERS);
         csvSchema = csvMapper
                 .schemaFor(Association.class)
@@ -60,6 +102,30 @@ public class RNAIndexer {
         }
     }
 
+    public void index(InputStream source) throws IOException {
+        try (BulkProcessor bulkProcessor = BulkProcessor
+                .builder(client, BULK_PROCESSOR_ADAPTER)
+                .setBulkActions(10000)
+                .setBulkSize(new ByteSizeValue(5, MB))
+                .setFlushInterval(timeValueSeconds(5))
+                .setConcurrentRequests(0)
+                .setBackoffPolicy(exponentialBackoff(timeValueMillis(100), 3))
+                .build()) {
+            ObjectMapper associationMapper = new ObjectMapper()
+                    .disable(CAN_OVERRIDE_ACCESS_MODIFIERS);
+            MappingIterator<Association> datas = createObjectReader(csvSchema.withHeader())
+                    .readValues(new InputStreamReader(source, forName("latin1")));
+            while (datas.hasNext()) {
+                Association association = datas.next();
+                byte[] bytes = associationMapper.writeValueAsBytes(association);
+                bulkProcessor.add(new IndexRequest("rna")
+                        .type("association")
+                        .id(association.getCodeGestionnaire())
+                        .source(bytes, JSON));
+            }
+        }
+    }
+
     @VisibleForTesting
     protected Association serializeOneLine(String s) throws IOException {
         return createObjectReader(csvSchema.withoutHeader()).readValue(s);
@@ -78,8 +144,9 @@ public class RNAIndexer {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
         File destination = File.createTempFile("rna", ".json");
-        InputStream source = RNAIndexer.class.getResourceAsStream("rna_import_20170301_.csv");
-        new RNAIndexer().convertToJson(new FileWriter(destination), source);
-        out.printf("%s", destination.getAbsolutePath());
+        try (InputStream source = RNAIndexer.class.getResourceAsStream("rna_import_20170301_.csv")) {
+            new RNAIndexer(getLocalHost()).index(source);
+            out.printf("%s", destination.getAbsolutePath());
+        }
     }
 }
--- src/test/java/RNAIndexerTest.java
+++ src/test/java/RNAIndexerTest.java
@@ -1,5 +1,12 @@
 import data.Association;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
+import pl.domzal.junit.docker.rule.DockerRule;
+import tool.ElasticsearchRule;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,15 +14,27 @@ import java.io.StringWriter;
 import java.net.URISyntaxException;
 import java.util.Calendar;
 
+import static java.net.InetAddress.getLocalHost;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RNAIndexerTest {
+
+    @ClassRule
+    public static DockerRule elasticsearch = ElasticsearchRule.ELASTICSEARCH;
+
+    private AssociationsIndex associationsIndex;
+
+    @Before
+    public void setUp() throws IOException {
+        associationsIndex = new AssociationsIndex(getLocalHost());
+        associationsIndex.createIndex();
+    }
 
     @Test
     public void should_read_one_import_association() throws IOException {
         Calendar calendar = Calendar.getInstance();
 
-        Association a = new RNAIndexer().serializeOneLine("" +
+        Association a = new RNAIndexer((Client) null).serializeOneLine("" +
                 "751P00085568;00085568;\"\";751P;1988-04-15;0001-01-01;D;S;INDIGO;dvpt et démocratisation de la musique;006030;000000;\"\";10        BD  Brune;\"\";75014;PARIS;75114;\"\";W751085568;R;\"\";2013-07-10 10:33:24.263074\n");
         assertThat(a.getCodeGestionnaire()).isEqualTo("751P00085568");
         assertThat(a.getAncienNumero()).isEqualTo("00085568");
@@ -54,70 +73,87 @@ public class RNAIndexerTest {
 
     @Test
     public void csv_should_have_23_columns() {
-        int columns = new RNAIndexer().countColumns();
+        int columns = new RNAIndexer((Client) null).countColumns();
 
         assertThat(columns).isEqualTo(23);
     }
 
     @Test
     public void should_migrate_a_file() throws IOException, URISyntaxException {
-        InputStream source = this.getClass().getResourceAsStream("rna_import_3_lines.csv");
-        StringWriter writer = new StringWriter();
+        try (InputStream source = this.getClass().getResourceAsStream("rna_import_3_lines.csv");
+             StringWriter writer = new StringWriter()) {
+            new RNAIndexer((Client) null).convertToJson(writer, source);
 
-        new RNAIndexer().convertToJson(writer, source);
+            assertThat(writer.toString()).isEqualTo("" +
+                    "{\"index\":{\"_id\":\"751P00085568\"}}\n" +
+                    " {" +
+                    "\"codeGestionnaire\":\"751P00085568\"," +
+                    "\"ancienNumero\":\"00085568\"," +
+                    "\"siret\":\"\"," +
+                    "\"codePrefectureOuSousPrefectureDuBureauGestionnaire\":\"751P\"," +
+                    "\"dateDeCreation\":577065600000," +
+                    "\"dateDePublication\":-62135769600000," +
+                    "\"nature\":\"D\"," +
+                    "\"groupement\":\"S\"," +
+                    "\"titre\":\"INDIGO\"," +
+                    "\"objet\":\"dvpt et démocratisation de la musique\"," +
+                    "\"objetSocialWaldec1\":\"006030\"," +
+                    "\"objetSocialWaldec2\":\"000000\"," +
+                    "\"adresseLigne1\":\"\"," +
+                    "\"adresseLigne2\":\"10        BD  Brune\"," +
+                    "\"adresseLigne3\":\"\"," +
+                    "\"codePostal\":\"75014\"," +
+                    "\"commune\":\"PARIS\"," +
+                    "\"codeInsee\":\"75114\"," +
+                    "\"siteWeb\":\"\"," +
+                    "\"observation\":\"W751085568\"," +
+                    "\"position\":\"R\"," +
+                    "\"numeroRUPMinistere\":\"\"," +
+                    "\"dateDeMiseAJour\":1373452404000" +
+                    "}\n" +
+                    " {\"index\":{\"_id\":\"751P00184848\"}}\n" +
+                    " {" +
+                    "\"codeGestionnaire\":\"751P00184848\"," +
+                    "\"ancienNumero\":\"00184848\"," +
+                    "\"siret\":\"\"," +
+                    "\"codePrefectureOuSousPrefectureDuBureauGestionnaire\":\"751P\"," +
+                    "\"dateDeCreation\":1200614400000," +
+                    "\"dateDePublication\":1203724800000," +
+                    "\"nature\":\"D\"," +
+                    "\"groupement\":\"S\"," +
+                    "\"titre\":\"COMITE DE GESTION RUE PINEL\"," +
+                    "\"objet\":\"la gestion des locaux et des services nécessaires aux buts des antennes et a l'exercice des activités de ses membres, conformément a la convention qui sera agree par l'association et l'association philosophique\"," +
+                    "\"objetSocialWaldec1\":\"006000\"," +
+                    "\"objetSocialWaldec2\":\"000000\"," +
+                    "\"adresseLigne1\":\"\"," +
+                    "\"adresseLigne2\":\"9         RUE Pinel\"," +
+                    "\"adresseLigne3\":\"\"," +
+                    "\"codePostal\":\"75013\"," +
+                    "\"commune\":\"PARIS\"," +
+                    "\"codeInsee\":\"75113\"," +
+                    "\"siteWeb\":\"\"," +
+                    "\"observation\":\"W751184848\"," +
+                    "\"position\":\"R\"," +
+                    "\"numeroRUPMinistere\":\"\"," +
+                    "\"dateDeMiseAJour\":1373988617000" +
+                    "}\n");
+        }
+    }
 
-        assertThat(writer.toString()).isEqualTo("" +
-                "{\"index\":{\"_id\":\"751P00085568\"}}\n" +
-                " {" +
-                "\"codeGestionnaire\":\"751P00085568\"," +
-                "\"ancienNumero\":\"00085568\"," +
-                "\"siret\":\"\"," +
-                "\"codePrefectureOuSousPrefectureDuBureauGestionnaire\":\"751P\"," +
-                "\"dateDeCreation\":577065600000," +
-                "\"dateDePublication\":-62135769600000," +
-                "\"nature\":\"D\"," +
-                "\"groupement\":\"S\"," +
-                "\"titre\":\"INDIGO\"," +
-                "\"objet\":\"dvpt et démocratisation de la musique\"," +
-                "\"objetSocialWaldec1\":\"006030\"," +
-                "\"objetSocialWaldec2\":\"000000\"," +
-                "\"adresseLigne1\":\"\"," +
-                "\"adresseLigne2\":\"10        BD  Brune\"," +
-                "\"adresseLigne3\":\"\"," +
-                "\"codePostal\":\"75014\"," +
-                "\"commune\":\"PARIS\"," +
-                "\"codeInsee\":\"75114\"," +
-                "\"siteWeb\":\"\"," +
-                "\"observation\":\"W751085568\"," +
-                "\"position\":\"R\"," +
-                "\"numeroRUPMinistere\":\"\"," +
-                "\"dateDeMiseAJour\":1373452404000" +
-                "}\n" +
-                " {\"index\":{\"_id\":\"751P00184848\"}}\n" +
-                " {" +
-                "\"codeGestionnaire\":\"751P00184848\"," +
-                "\"ancienNumero\":\"00184848\"," +
-                "\"siret\":\"\"," +
-                "\"codePrefectureOuSousPrefectureDuBureauGestionnaire\":\"751P\"," +
-                "\"dateDeCreation\":1200614400000," +
-                "\"dateDePublication\":1203724800000," +
-                "\"nature\":\"D\"," +
-                "\"groupement\":\"S\"," +
-                "\"titre\":\"COMITE DE GESTION RUE PINEL\"," +
-                "\"objet\":\"la gestion des locaux et des services nécessaires aux buts des antennes et a l'exercice des activités de ses membres, conformément a la convention qui sera agree par l'association et l'association philosophique\"," +
-                "\"objetSocialWaldec1\":\"006000\"," +
-                "\"objetSocialWaldec2\":\"000000\"," +
-                "\"adresseLigne1\":\"\"," +
-                "\"adresseLigne2\":\"9         RUE Pinel\"," +
-                "\"adresseLigne3\":\"\"," +
-                "\"codePostal\":\"75013\"," +
-                "\"commune\":\"PARIS\"," +
-                "\"codeInsee\":\"75113\"," +
-                "\"siteWeb\":\"\"," +
-                "\"observation\":\"W751184848\"," +
-                "\"position\":\"R\"," +
-                "\"numeroRUPMinistere\":\"\"," +
-                "\"dateDeMiseAJour\":1373988617000" +
-                "}\n");
+    @Test
+    public void should_index_2_docs() throws IOException {
+        try (InputStream source = this.getClass().getResourceAsStream("rna_import_3_lines.csv")) {
+            new RNAIndexer(getLocalHost()).index(source);
+        }
+
+        associationsIndex.getClient().admin().indices().prepareRefresh().get();
+        SearchResponse response = associationsIndex.getClient().prepareSearch().get();
+
+        assertThat(response.getHits().getHits()).isNotEmpty();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        associationsIndex.deleteIndexIfExists();
     }
 }
